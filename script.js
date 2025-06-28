b7071821411d65b9a09901100d813de0c0f4c3fe
Ansh Mishra <ansh08672@gmail.com>
2025-06-28 17:04:16 +0530

    bann gayi

---
+++ script.js
@@ -0,0 +1,35 @@
+function clicked(){
+    location.href='menu.html';
+};
+
+function chinese(){
+    location.href='chinese.html';
+};
+function touch(){
+    location.href='south.html';
+};
+
+function goToNorthIndian(){
+    location.href='north-indian.html';
+};
+
+function goToSnacks(){
+    location.href='snacks.html';
+};
+
+function goToJuice(){
+    location.href='juice.html';
+};
+
+function goToItalian(){
+    location.href='italian.html';
+};
+
+function goToMexican(){
+    location.href='mexican.html';
+};
+
+function goToPunjabi(){
+    location.href='punjabi.html';
+};
+
